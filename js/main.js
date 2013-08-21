ccbe9c070081dc98c3c221baa63943efebd6cab9
David Potsiadlo <davidpots@gmail.com>
2013-08-21 07:49:31 -0400

    Removes parallax on small screens. Copy tweak to homepage project descriptions.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 // Parallax scroll for homepage
 $(document).scroll(function(){
-	s = $(document).scrollTop();
-	$('.home-intro .content').css('-webkit-transform', 'translateY(' + (s/3) + 'px)');
+  if ($(window).width() > 1028) {
+  	s = $(document).scrollTop();
+  	$('.home-intro .content').css('-webkit-transform', 'translateY(' + (s/3) + 'px)');
+  }
 });
 
+// FitText! To ensure certain text elements fit on one line. via http://fittextjs.com/
 $(document).ready(function(){
   jQuery(".home-intro h1").fitText(1.0, { minFontSize: '20px', maxFontSize: '48px' });
+  jQuery(".home-main p.sub").fitText(1.0, { minFontSize: '12px', maxFontSize: '17px' });
 });
