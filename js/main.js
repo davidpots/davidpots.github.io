623bf2295d441c5410abad9f4ea34b424af6359e
David Potsiadlo <davidpots@gmail.com>
2013-10-07 08:08:03 -0400

    Removes fittext for the project sub-headers

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // FitText! To ensure certain text elements fit on one line. via http://fittextjs.com/
 $(document).ready(function(){
   jQuery(".home-intro h1").fitText(0.8, { minFontSize: '12px', maxFontSize: '54px' });
-  jQuery(".home-main p.sub").fitText(1.0, { minFontSize: '11px', maxFontSize: '19px' });
 });
