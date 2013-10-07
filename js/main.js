9a53a11de8fb18838d55c7125c6e41858ac59703
David Potsiadlo <davidpots@gmail.com>
2013-10-07 07:56:28 -0400

    Quick style refresh to davidpots.com ... gets rid of the orange / pyramid, typographic update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
-// Parallax scroll for homepage
-$(document).scroll(function(){
-  if ($(window).width() > 1028) {
-  	s = $(document).scrollTop();
-  	$('.home-intro .content').css('-webkit-transform', 'translateY(' + (s/3) + 'px)');
-  }
-});
-
 // FitText! To ensure certain text elements fit on one line. via http://fittextjs.com/
 $(document).ready(function(){
-  jQuery(".home-intro h1").fitText(1.0, { minFontSize: '12px', maxFontSize: '48px' });
-  jQuery(".home-main p.sub").fitText(1.0, { minFontSize: '12px', maxFontSize: '17px' });
+  jQuery(".home-intro h1").fitText(1.0, { minFontSize: '12px', maxFontSize: '54px' });
+  jQuery(".home-main p.sub").fitText(1.0, { minFontSize: '12px', maxFontSize: '19px' });
 });
